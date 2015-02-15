3eab47500f58e957bae8a443586d30326d384899
Gullern <jangu@stud.ntnu.no>
2015-02-15 17:35:40 +0100

    Working on assignment 1.

--- assignments/a1/langevin.f
+++ assignments/a1/langevin.f
@@ -10,60 +10,158 @@ program langvein
 
     implicit none
 
-    ! PARAMETERS
-    real(wp), parameter     :: r1       = 0.000000012_wp
-    real(wp), parameter     :: alpha    = 0.2_wp
-    real(wp), parameter     :: L        = 0.000020_wp
-    real(wp), parameter     :: eta      = 0.001_wp
-    real(wp), parameter     :: kB_T     = 0.026_wp
-    real(wp), parameter     :: DU       = 80.0_wp
+    ! ########### PARAM ############
 
+    ! -- Acuracy
+    ! 
+    ! dt = dt0 * accuracy, where dt0 is the critical
+    ! time step value. 
+    real(wp), parameter     :: accuracy     = 1E-4_wp
+
+    ! -- Standard values
+    real(wp), parameter     :: std_r1       = 12E-9_wp
+    real(wp), parameter     :: std_alpha    = 0.2_wp
+    real(wp), parameter     :: std_L        = 20E-6_wp
+    real(wp), parameter     :: std_eta      = 1E-3_wp
+    real(wp), parameter     :: std_kB_T     = 26E-3_wp
+    real(wp), parameter     :: std_DU       = 80.0_wp
+    real(wp), parameter     :: std_x0       = 0.0_wp
+    real(wp), parameter     :: std_period   = 0.0_wp
+
+    ! -- Parameters
+    real(wp)                :: r1
+    real(wp)                :: alpha
+    real(wp)                :: L
+    real(wp)                :: eta
+    real(wp)                :: kB_T
+    real(wp)                :: DU
+    real(wp)                :: x0
+    real(wp)                :: period
+
+    ! -- Derived parameters
     real(wp)                :: gamma1
     real(wp)                :: omega
     real(wp)                :: diff
-
     real(wp)                :: dt
-    real(wp)                :: period
 
     ! FILE
-    integer, parameter :: output_fid = 20
+    character(len=*), parameter :: file_name    = 'diffusion.dat'
+    integer, parameter          :: output_fid   = 20
 
-    ! ALLOC
-    integer     :: i
-    real(wp)    :: x
-    real(wp)    :: t
+    ! VARS
+    real(wp) :: max_force
+    real(wp) :: K0
+
+    ! ########## END DECL ########
 
     ! Seed random
     call init_random_seed
 
-    gamma1  = 6 * PI * eta * r1
+    ! Get input
+    call input()
+
+    ! Calculate parameters
+    gamma1  = 6.0_wp * PI * eta * r1
     omega   = DU / (gamma1 * L ** 2)
     diff    = kB_T / DU
 
+    ! Find dt
+    max_force = MAX(1.0_wp / alpha, 1.0_wp / ( 1.0_wp - alpha))
+    K0 = (max_force * alpha ** 2 / (128.0_wp * sqrt(2.0_wp * diff) * diff)) ** ( 1.0_wp / 3.0_wp)
+    K0 = MAX(k0, 1.0_wp)
+    dt = alpha ** 2 / (128.0_wp * diff * K0 ** 2)
+    dt = dt * accuracy
+    !print *, dt
 
-
-    print*,PI
-    print*,DACOS(-1.0_wp)
-    print*,2.0_wp * DASIN(1.0_wp)
-
-
-
-    open(output_fid, file='gauss.dat')
-    do i = 1, 1000
-        write(20, *) rand_gauss()
-    end do
-
-    close(output_fid)
-
-
-    ! Euler
-    do i = 1, 100
-        x = x - 1.0_wp / gamma1 * force(x, t) * dt + SQRT(2 * kB_T * dt / gamma1)
-
-    end do
-
+    ! Begin Euler scheme
+    call euler()
 
 contains
+
+    ! 
+    ! Euler scheme
+    ! 
+    ! Finds path determined by ODE
+    ! by iteration. 
+    ! 
+    subroutine euler()
+        ! ALLOC
+        integer     :: i
+        real(wp)    :: x
+        real(wp)    :: t
+
+        ! Initiate in reduced units
+        x = x0 / L
+        t = 0.0_wp
+
+        open(output_fid, file=file_name)
+        write(output_fid, *) x
+        do i = 1, 10000
+            !x = x - force(x, t) * dt + SQRT(2 * diff * dt) * rand_gauss()
+            x = x + SQRT(2.0_wp * diff * dt) * rand_gauss()
+            t = t + dt
+            write(output_fid, *) x
+        end do
+
+        close(output_fid)
+    end subroutine
+
+    ! 
+    ! Read input parameters
+    !
+    ! Reads all relevant input parameters 
+    ! from terminal.
+    ! 
+    subroutine input()
+        character(len=*), parameter :: format_0 = "(A)"
+        character(len=*), parameter :: format_1 = "(A, F14.10, A)"
+
+        write(*, format_0) 'Input parameters:'
+
+        write(*, format_1, advance='no') 'Potential strength DeltaU (', std_DU, '):'
+        call input_w_default(DU, std_DU)
+        write(*, format_1, advance='no') 'Initial particle position x0 (', std_x0, '):'
+        call input_w_default(x0, std_x0)
+        write(*, format_1, advance='no') 'Time period tau (', std_period, '):'
+        call input_w_default(period, std_period)
+        write(*, format_1, advance='no') 'Particle radius r1 (', std_r1, '):'
+        call input_w_default(r1, std_r1)
+        write(*, format_1, advance='no') 'Potential asymmetry factor alpha (', std_alpha, '):'
+        call input_w_default(alpha, std_alpha)
+        write(*, format_1, advance='no') 'Potential periodic length L (', std_L, '):'
+        call input_w_default(L, std_L)
+        write(*, format_1, advance='no') 'Unknown constant eta (', std_eta, '):'
+        call input_w_default(eta, std_eta)
+        write(*, format_1, advance='no') 'Temperature energy kB_T (', std_kB_T, '):'
+        call input_w_default(kB_T, std_kB_T)
+    end subroutine
+
+    ! 
+    ! Inputs from console with default
+    ! 
+    ! Reads a single line from standard input. If 
+    ! the input is a valid number, param is set to
+    ! it. If not, param is set to default_param. 
+    ! 
+    subroutine input_w_default(param, default_param)
+        real(wp), intent(out) :: param
+        real(wp), intent(in)  :: default_param
+
+        integer, parameter              :: MAX_LENGTH   = 100
+        character(len=*), parameter     :: format_0     = "(A)"
+        character(len=MAX_LENGTH)       :: input_str
+        real(wp)                        :: input_real
+        integer                         :: flag
+
+        read(*, format_0) input_str
+
+        read(input_str, *, iostat=flag) input_real
+        if (flag == 0) then
+            param = input_real
+        else
+            param = default_param
+        end if
+    end subroutine
 
     ! 
     ! Potential U(x, t)
@@ -132,7 +230,7 @@ contains
         real(wp)    :: u2
         real(wp)    :: s
 
-        s = 0
+        s = 0.0_wp
         do while(s == 0 .or. s >= 1)
             call random_number(u1)
             call random_number(u2)
@@ -142,8 +240,6 @@ contains
         end do
 
         rand_gauss = u1 * SQRT( - 2.0_wp * LOG(s) / s)
-
-        return
     end function
 
 end program
--- assignments/a1/visual.py
+++ assignments/a1/visual.py
@@ -1,28 +1,57 @@
+#!/usr/bin/env python
+
 # Reads and plots data from:
-file_name = 'gauss.dat';
+file_name = 'diffusion';
 
 import numpy
 from numpy import linspace
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg');
 import matplotlib.pyplot as plt
 from matplotlib import rc
-rc('text', usetex=True)
+rc('text', usetex=True);
 
-def main():
-    fid = open(file_name, 'r');
+def graph():
+    (length, data) = read_data();
+    plt.figure();
+    x = linspace(0, length - 1, length);
+    y = data;
+    plt.plot(x, y);
+    plt.savefig(file_name + '.png');
 
+def graph_r():
+    (length, data) = read_data();
+    plt.figure();
+    y = linspace(0, length - 1, length);
+    x = data;
+    plt.plot(x, y);
+    plt.savefig(file_name + '.png');
+
+def hist():
+    (length, data) = read_data();
+    bins = 20;
+    plt.figure();
+    plt.hist(data, bins = bins);
+    #plt.axis([0, length, 0, iterations]);
+    #plt.xlabel('$x$');
+    #plt.ylabel('Count time');
+    plt.savefig(file_name + '.png');
+
+def read_data():
+    fid = open(file_name + '.dat', 'r');
     lines = fid.readlines();
     length = len(lines);
     data = [float(line.strip()) for line in lines];
+    print('lines: ' + str(length));
+    return (length, data);
 
-    bins = 20;
-    plt.figure(figsize=(5,5));
-    plt.hist(data, bins = bins);
-    #plt.axis([0, length, 0, iterations])
-    #plt.xlabel('$x$');
-    #plt.ylabel('Count time');
-    plt.savefig('gauss.png');
+def main():
+    graph_r();
+
+import sys
+print sys.argv;
+
+print locals()['graph'];
 
 main();
 
